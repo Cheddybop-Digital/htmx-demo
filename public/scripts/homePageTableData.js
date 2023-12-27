9fc05e0316f6d3d137ee842b51e20c95450dd9b9
lstew111 <loren.stewart@engelvoelkers.com>
2023-12-26 19:05:58 -0800

    made code more effective with less code

--- public/scripts/homePageTableData.js
+++ public/scripts/homePageTableData.js
@@ -1,14 +1,10 @@
 // * Create a namespace on the window and assign to a convenient variable
 tableData = window.tableData;
 
-const startingSort = "";
-
 // * add data and functions/behavior to the namespace
 tableData = {
-  _sortDir: startingSort,
+  _sortDir: "",
   _sortField: "",
-  _searchField: "",
-  _searchTerm: "",
 
   get sortDir() {
     return this._sortDir;
@@ -22,60 +18,40 @@ tableData = {
   get sortField() {
     return this._sortField;
   },
-  get searchField() {
-    return this._searchField;
-  },
-  set searchField(field) {
-    this._searchField = field;
-  },
-  get searchTerm() {
-    return this._searchTerm;
-  },
-  set searchTerm(val) {
-    this._searchTerm = val;
-  },
-  toggleSort() {
-    switch (this._sortDir) {
+  toggleSort(field) {
+    if (field !== this.sortField) {
+      // if the sort field is not the same as the last one
+      // then reset the sort direction steps
+      this.sortDir = "";
+      this.sortField = field;
+    }
+    switch (this.sortDir) {
       case "asc":
-        this._sortDir = "desc";
+        this.sortDir = "desc";
         return this.sortDir;
       case "desc":
-        this._sortDir = startingSort;
+        this.sortDir = "";
         return this.sortDir;
-      case startingSort:
-        this._sortDir = "asc";
+      case "":
+        this.sortDir = "asc";
         return this.sortDir;
       default:
         return this.sortDir;
     }
   },
-  toggleSearchInput(className) {
-    const node = document.querySelector(`.th-container-${className}`);
-    if (node.classList.contains("show-input")) {
-      node.classList.remove("show-input");
+  toggleSearchInput(field) {
+    const node = document.querySelector(`.th-container-${field} input`);
+    if (node.classList.contains("hide")) {
+      node.classList.remove("hide");
     } else {
-      node.classList.add("show-input");
+      node.classList.add("hide");
     }
   },
   searchIconClickHandler(field) {
-    this._searchField = field;
     this.toggleSearchInput(field);
-  },
-  submitHandler(event, field) {
-    event.preventDefault();
-
-    // set the search term, so it isn't lost when sorting changes
-    tableData._searchTerm = Array.from(document.forms).find(
-      (f) => f.name === `search-form-${field}`,
-    )?.searchTerm?.value;
-
-    // set the sortField
-    tableData._sortField = field;
   },
   clearFilters() {
     // clear data
-    this._searchTerm = "";
-    this._searchField = "";
     this._sortDir = "";
     this._sortField = "";
 
@@ -85,9 +61,3 @@ tableData = {
     window.location.replace(`${url.origin}/users`);
   },
 };
-
-addEventListener("DOMContentLoaded", (event) => {
-  // TODO: parse url search params and set their values on page load
-});
-
-// total=`${Math.ceil(<%= it.count %>/<%= it.limit %>))}`
